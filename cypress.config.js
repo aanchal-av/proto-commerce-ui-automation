3706ea3b53774573aecd37845346ebf04f3999e1
Aanchal <aanchalverma.av@outlook.com>
2025-02-20 14:20:04 +0530

    Added test cases for page object design

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("Cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 4000,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/*.js',
   },
 });
--- cypress/e2e/checkout-page.cy.js
+++ cypress/e2e/checkout-page.cy.js
@@ -1,51 +1,37 @@
 /// <reference types="Cypress" />
 
-describe('Login page validation', () => {
+import LoginPage from "../support/pageObjects/LoginPage"
+import CheckoutPage from "../support/pageObjects/CheckoutPage"
 
+describe('placing order', () => {
+  const loginpage=new LoginPage()
+  const checkoutpage= new CheckoutPage()
 
-it('Add product to cart', () => {
-    const productName='iphone X'
-    let sum = 0
-    cy.visit('https://rahulshettyacademy.com/loginpagePractise/')
-    cy.get('#username').type('rahulshettyacademy')
-    cy.get('#password').type('learning')
-    cy.get('#terms').check()
-    cy.get('#signInBtn').click()
-    cy.get('app-card').should('have.length',4)
-    cy.get('app-card').filter(`:contains("${productName}")`).then($el=>{
-      cy.wrap($el).should('have.length', 1)
-      cy.wrap($el).contains('button','Add').click()
-
-    })
-    cy.get('app-card').eq(1).click()
-    cy.contains('a','Checkout').click()
-    cy.get('tr td:nth-child(4) strong').each(($el)=>{
-      
-      const price=Number($el.text().split(" ")[1].trim())
-      sum = sum + price
-    }).then(()=>{
-      expect(sum).to.be.lessThan(200000)
-    })
+  beforeEach(function(){
+    // runs once before all test in this block
+    cy.fixture('users.json').as('data')
   })
 
-it('place an order',()=>{
+it('Add product to cart', function() {
     const productName='iphone X'
-    cy.visit('https://rahulshettyacademy.com/loginpagePractise/')
-    cy.get('#username').type('rahulshettyacademy')
-    cy.get('#password').type('learning')
-    cy.get('#terms').check()
-    cy.get('#signInBtn').click()
-    cy.get('app-card').filter(`:contains("${productName}")`).then($el=>{
-        cy.wrap($el).should('have.length', 1)
-        cy.wrap($el).contains('button','Add').click()
-      })
+    loginpage.goTo('https://rahulshettyacademy.com/loginpagePractise/')
+    loginpage.login(this.data.username,this.data.password)
+    checkoutpage.productCard({timeout:5000})
+    checkoutpage.clickAddToCart(productName)
+    cy.get('app-card').eq(2).click()
+    cy.contains('a','Checkout').click()
+    checkoutpage.sumofProducts()  
+  })
+
+it('place an order',function(){
+    const productName='iphone X'
+    loginpage.goTo('https://rahulshettyacademy.com/loginpagePractise/')
+    loginpage.login(this.data.username,this.data.password)
+    checkoutpage.clickAddToCart(productName)
       cy.get('app-card').eq(1).click()
       cy.contains('a','Checkout').click()
       cy.contains('button','Checkout').click()
-      cy.get('#country').type('India')
-       
-      cy.get('.suggestions ul li a',{timeout:20000}).click()
-      cy.get('.btn-success').click() 
+      cy.submitFormDetails()
       cy.get('.alert').should('contain',' Thank you! Your order will be delivered in next few weeks :-)')
 
 })
