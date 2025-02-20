3916346addf4f421eaae06b00294eb241f46094e
Aanchal <aanchalverma.av@outlook.com>
2025-02-20 15:42:48 +0530

    Added reporting and set up env variables

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,23 @@
 const { defineConfig } = require("Cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 4000,
+  env:{
+       url: 'https://rahulshettyacademy.com/loginpagePractise/'
+ },
 
+  defaultCommandTimeout: 4000,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/*.js',
