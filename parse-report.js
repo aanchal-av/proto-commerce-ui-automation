657c4caa8b62f40129e1d985282092ee0c10a364
Aanchal <aanchalverma.av@outlook.com>
2025-07-04 22:20:51 +0530

    updated parse-reporter.js file

--- parse-report.js
+++ parse-report.js
@@ -1,10 +1,15 @@
 const fs = require('fs')
 
 const reportUrl = 'https://aanchal-av.github.io/proto-commerce-ui-automation/index.html';
+const resultJson=JSON.parse(fs.readFileSync('cypress/reports/index.json'))
 
 const message = `
 🧪 *Cypress Test Summary*
 📥 [Click to View HTML Report](${reportUrl})
+🧮 Total Tests: ${resultJson.totalTests}
+🥳 Passed Tests: ${resultJson.totalPassed}
+🫠 Pending Tests: ${resultJson.totalPending}
+😱 Failed Tests: ${resultJson.totalFailed}
 `;
 
 fs.writeFileSync("slack-message.txt", JSON.stringify({ text: message.trim() }, null, 2));
