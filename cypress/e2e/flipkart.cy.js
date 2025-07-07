91ee610154d7e535920670498bb96af7910d96d4
Aanchal <aanchalverma.av@outlook.com>
2025-07-07 13:08:02 +0530

    updated

--- cypress/e2e/flipkart.cy.js
+++ cypress/e2e/flipkart.cy.js
@@ -1,17 +1,17 @@
-///<reference types="cypress"/>
+// ///<reference types="cypress"/>
 
 
-describe('validate the menu items',()=>{
-    beforeEach(()=>{
-        cy.visit('https://www.flipkart.com/')
-    })
-    it('validate the selection of mobile item',()=>{
-        cy.get('.YBLJE4').each(($el)=>{
-                 const menuItem=$el.text().trim()
-                 cy.log(menuItem)
-                   if(menuItem==='Mobiles'){
-              cy.wrap($el).click()
-                    }
-        })
-    })
-})
+// describe('validate the menu items',()=>{
+//     beforeEach(()=>{
+//         cy.visit('https://www.flipkart.com/')
+//     })
+//     it('validate the selection of mobile item',()=>{
+//         cy.get('.YBLJE4').each(($el)=>{
+//                  const menuItem=$el.text().trim()
+//                  cy.log(menuItem)
+//                    if(menuItem==='Mobiles'){
+//               cy.wrap($el).click()
+//                     }
+//         })
+//     })
+// })
