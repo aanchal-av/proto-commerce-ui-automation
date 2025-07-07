cfc2413b557b1d0cf37ce7d9c7f62ab7e0f95614
Aanchal <aanchalverma.av@outlook.com>
2025-07-07 12:08:37 +0530

    updated

--- cypress/e2e/flipkart.cy.js
+++ cypress/e2e/flipkart.cy.js
@@ -1,9 +1,10 @@
 ///<reference types="cypress"/>
 
-beforeEach(()=>{
-    cy.visit('https://www.flipkart.com/')
-})
+
 describe('validate the menu items',()=>{
+    beforeEach(()=>{
+        cy.visit('https://www.flipkart.com/')
+    })
     it('validate the selection of mobile item',()=>{
         cy.get('.YBLJE4').each(($el)=>{
                  const menuItem=$el.text().trim()
